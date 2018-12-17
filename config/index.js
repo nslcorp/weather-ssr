b518ec8ff58b868bd337ef4997ba3211ad80bb32
Nikitin <botodnepr@gmail.com>
2018-12-17 23:22:46 +0200

    hide variables

--- config/index.js
+++ config/index.js
@@ -1,6 +1,4 @@
 const config = process.env.NODE_ENV === 'production' ? require('./prod') : require('./dev')
-console.log(process.env.NODE_ENV === 'production')
-console.log(config)
 
 module.exports = {
   apiKey: config.apiKey
